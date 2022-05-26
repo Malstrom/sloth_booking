77491664236349f2828e1ba46beb54271be8174f
Igor Mironenko <igormir87@gmail.com>
2022-05-26 03:11:48 +0300

    init

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,14 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+if (toastTrigger) {
+  toastTrigger.addEventListener('click', () => {
+    const toast = new bootstrap.Toast(toastLiveExample)
+
+    toast.show()
+  })
+}
