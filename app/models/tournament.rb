97d32b876284ec568dbdb9a0cecce85934117b78
Igor Mironenko <igormir87@gmail.com>
2022-06-03 02:54:57 +0300

    create validation of presence

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,3 +1,5 @@
 class Tournament < ApplicationRecord
   has_many :timecells, as: :bookable
+
+  validates_presence_of :name, :rating, :price
 end
--- app/models/training.rb
+++ app/models/training.rb
@@ -1,3 +1,7 @@
 class Training < ApplicationRecord
   has_many :timecells, as: :bookable
+
+  validates_presence_of :name, :trainer, :price
+
+  private
 end
