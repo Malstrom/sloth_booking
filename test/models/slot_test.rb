951889b9dee599b3ecd43f009b809e97665512e0
Igor Mironenko <igormir87@gmail.com>
2022-06-03 14:04:47 +0300

    create test for model

--- test/models/slot_test.rb
+++ test/models/slot_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class SlotTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @slot = Slot.new(gametable: Gametable.first, time: "14:00".to_time, price: 400, state: :open)
+  end
+
+  test 'valid user' do
+    assert @slot.valid?
+  end
+
+  test 'invalid without name' do
+    @slot.gametable = nil
+    refute @slot.valid?, 'saved user without a gametable'
+    assert_not_nil @slot.errors[:gametable], 'no validation error for gametable present'
+  end
+
+  test 'invalid without email' do
+    @user.email = nil
+    refute @user.valid?
+    assert_not_nil @user.errors[:email]
+  end
+
 end
