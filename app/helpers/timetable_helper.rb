4925b7ca14d217e88b6ad4d6cc63aadd493b969c
Igor Mironenko <igormir87@gmail.com>
2022-06-09 15:41:42 +0300

    create helper to define color of days

--- app/helpers/timetable_helper.rb
+++ app/helpers/timetable_helper.rb
@@ -1,3 +1,11 @@
 module TimetableHelper
-
+  def define_day_color(selected_day, day)
+    if day == selected_day
+      "link-success fw-bold link-date"
+    elsif day.on_weekend?
+      "text-danger link-date"
+    else
+      "link-secondary link-date"
+    end
+  end
 end
