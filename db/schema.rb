c5558e2c135638534b8cff732766656eb247f055
Michael <overbeat@mail.ru>
2022-06-11 01:01:53 +0300

    Better torunaments and trainings view Timetable updated (now scrolled at the place) BG-image for login size reduced

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_02_132100) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_09_132733) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
