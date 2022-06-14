fbe2e007f509823a569c8e0e95f5e4223dc7083a
Igor Mironenko <igormir87@gmail.com>
2022-06-14 15:15:25 +0300

    refactor slot

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 class Slot < ApplicationRecord
   belongs_to :gametable
   belongs_to :bookable, polymorphic: true, optional: true
@@ -11,6 +10,8 @@ class Slot < ApplicationRecord
   before_update :toggle_bookable
 
   validates :time, comparison: { greater_than: Time.now }, on: :update
+
+  INTERVAL = 30.minutes
 
   scope :by_club, ->(club) { joins(:gametable).where('gametables.club_id = ?', club).order(:gametable_id, :time) }
   scope :open_slot, -> { where(state: :open) }
@@ -33,44 +34,33 @@ class Slot < ApplicationRecord
     case bookable_type
     when 'Training' then bookable.trainer
     when 'Tournament' then "< #{bookable.rating}"
+    when 'Event' then bookable.email
     else
       price
     end
   end
 
   def define_color
-    if bookable_type.nil?
-      case price.to_i
-      when 0..300    then 'cell-color-yellow'
-      when 301..450  then 'cell-color-blue'
-      when 451..600  then 'cell-color-green'
-      when 601..750  then 'cell-color-pink'
-      when 751..3000 then 'cell-color-purple'
-      else 'cell-color-yellow'
-      end
+    if bookable_id?
+      define_color_by_bookable
     else
-      bookable_type == 'Training' ? 'cell-color-training' : 'cell-color-tournament'
+      define_color_by_price
     end
   end
 
-  def self.generate_slots(selected_day, club_id)
-    club = Club.find club_id
-    club_start = DateTime.parse(club.starts_at)
-    club_end = DateTime.parse(club.ends_at)
-    starts = selected_day.beginning_of_day
-    ends = selected_day.end_of_day
-
-    hours = (starts.to_i..ends.to_i).step(30.minutes).map { |hour| Time.zone.at(hour) }
-
-    club.gametables.each do |gametable|
-      hours.each do |hour|
-        if hour.strftime('%H:%M') < club_start.strftime('%H:%M') || hour.strftime('%H:%M').to_s >= club_end.strftime('%H:%M')
-          gametable.slots.build(time: hour, price: 400).close!
-        else
-          gametable.slots.build(time: hour, price: 400).open!
-        end
-      end
+  def define_color_by_price
+    case price.to_i
+    when 0..300    then 'cell-color-yellow'
+    when 301..450  then 'cell-color-blue'
+    when 451..600  then 'cell-color-green'
+    when 601..750  then 'cell-color-pink'
+    when 751..3000 then 'cell-color-purple'
+    else 'cell-color-yellow'
     end
+  end
+
+  def define_color_by_bookable
+    bookable_type == 'Training' or bookable_type == 'Event' ? 'cell-color-training' : 'cell-color-tournament'
   end
 
   def self.update_working_date(club, selected_day, starts_at, ends_at)
@@ -89,32 +79,55 @@ class Slot < ApplicationRecord
     update(bookable_type: nil, bookable_id: nil)
   end
 
-  def self.available_slots(club, selected_day = nil, duration = nil)
+  def self.available_slots(club, selected_day, duration)
     selected_day ||= Date.tomorrow
     duration     ||= 1
 
-    open_slots          = Slot.by_club(club).open_slot.by_day(selected_day)
     duration_in_minutes = duration.to_i.hours.to_i / 60
-    closing_time        = open_slots.group_by_hours.keys.last + 30.minutes
-    booked_slots        = open_slots.booked.by_day(selected_day).group_by_hours
+    slots = Slot.by_club(club).by_day(selected_day)
 
-    not_available_times = times_not_bookable(unavailable_hours(booked_slots), closing_time, duration_in_minutes)
+    open_slots = slots.open_slot
 
-    open_slots.only_available(not_available_times)
+    generate_slots(selected_day, club.id) if slots.empty?
+    open_slots.only_available(unavailable_times(open_slots, duration_in_minutes))
   end
 
-  def self.unavailable_hours(booked_slots)
-    booked_slots.map { |time| time.first if time.last.count >= time.last.first.gametable.club.gametables.count }.compact
+  def self.unavailable_times(open_slots, duration_in_minutes)
+    closing_time = open_slots.group_by_hours.keys.last + INTERVAL
+    booked_slots = open_slots.booked.group_by_hours
+
+    booked_time_range = booked_slots.map { |time| time.first if time.last.count >= time.last.first.gametable.club.gametables.count }.compact
+
+    times_not_bookable(booked_time_range, closing_time, duration_in_minutes)
   end
 
   def self.times_not_bookable(booked_times, closing_time, duration_in_minutes)
     not_available_times = []
     booked_times.append(closing_time).each do |time|
       start = time - duration_in_minutes.minutes
-      start = start + 30.minutes if duration_in_minutes.minutes > 30.minutes
-      (start.to_i..time.to_i).step(30.minutes).map { |t| not_available_times << Time.at(t) }
+      start += INTERVAL if duration_in_minutes.minutes > INTERVAL
+      (start.to_i..time.to_i).step(INTERVAL).map { |t| not_available_times << Time.at(t) }
     end
     not_available_times
+  end
+
+  def self.generate_slots(selected_day, club_id)
+    club = Club.find club_id
+    hours = time_interval_in_day(selected_day)
+
+    club.gametables.each do |gametable|
+      hours.each do |hour|
+        if hour.strftime('%H:%M') < club.starts_at.strftime('%H:%M') || hour.strftime('%H:%M').to_s >= club.ends_at.strftime('%H:%M')
+          gametable.slots.build(time: hour, price: 400).close!
+        else
+          gametable.slots.build(time: hour, price: 400).open!
+        end
+      end
+    end
+  end
+
+  def self.time_interval_in_day(selected_day)
+    (selected_day.beginning_of_day.to_i..selected_day.end_of_day.to_i).step(INTERVAL).map { |hour| Time.at(hour) }
   end
 
   private
