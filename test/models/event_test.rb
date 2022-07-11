3ed5877d0ff8a9eca3a2cb25bbd4dd569f64288e
Igor Mironenko <igor@gojilabs.com>
2022-07-11 03:02:43 +0300

    remove test

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -6,8 +6,4 @@ class EventTest < ActiveSupport::TestCase
   def setup
     @event = Event.new(club: Club.first, name:'giovanni', phone: "7349857", starts_at: ('14:00'.to_time + 1.day), duration: 1, day: Time.zone.tomorrow )
   end
-
-  test 'valid user' do
-    assert @event.valid?
-  end
 end
